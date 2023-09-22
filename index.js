51358d40e25da15031e7cb93b429009c443d4740
Satyam Singh Virat <satyam23599@gmail.com>
2023-09-22 16:10:17 +0530

    minor logic changes & comments added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Initialize variables for time components
 let initialSeconds = 0;
 let initialMiliSeconds = 0;
 let initialMinutes = 0;
 
+// Select HTML elements
 let Minutes = document.querySelector('.Minutes');
 let Seconds = document.querySelector('.Seconds');
 let MiliSeconds = document.querySelector('.MiliSeconds');
@@ -10,55 +12,62 @@ let start = document.querySelector('.start');
 let stop = document.querySelector('.stop');
 let reset = document.querySelector('.reset');
 
-let timer =null;
+let timer = null;
 
-start.addEventListener('click', ()=>{
-    if (timer === null) { 
+// Start button click event listener
+start.addEventListener('click', () => {
+    // Start the timer if it's not already running
+    if (timer === null) {
         timer = setInterval(startWatch, 10);
     }
 })
 
-stop.addEventListener('click', ()=>{
+// Stop button click event listener
+stop.addEventListener('click', () => {
+    // Pause the timer by clearing the interval
     clearInterval(timer);
     timer = null;
 })
 
-reset.addEventListener('click',() =>{
+// Reset button click event listener
+reset.addEventListener('click', () => {
+    // Reset the timer and all time components to zero
     clearInterval(timer);
     timer = null;
-
-    initialMinutes=0;
-    initialSeconds=0;
-    initialMiliSeconds=0;
+    initialMinutes = 0;
+    initialSeconds = 0;
+    initialMiliSeconds = 0;
     
-    Minutes.innerHTML = '0'+initialMinutes;
-    Seconds.innerHTML = '0'+ initialSeconds;
-    MiliSeconds.innerHTML = '0'+ initialMiliSeconds;
+    // Update the displayed time to show zeros
+    Minutes.innerHTML = '0' + initialMinutes;
+    Seconds.innerHTML = '0' + initialSeconds;
+    MiliSeconds.innerHTML = '0' + initialMiliSeconds;
 })
 
-function startWatch(){
+// Function to update the timer logic
+function startWatch() {
     initialMiliSeconds++;
-    if(initialMiliSeconds<=9){
-        MiliSeconds.innerHTML = '0' + initialMiliSeconds;
-    }
-    if(initialMiliSeconds>9){
-        MiliSeconds.innerHTML = initialMiliSeconds;
-    }
-    if(initialMiliSeconds>99){
+    
+    // Update milliseconds display with leading zero
+    MiliSeconds.innerHTML = initialMiliSeconds <= 9 ? '0' + initialMiliSeconds : initialMiliSeconds;
+    
+    if (initialMiliSeconds > 99) {
         initialSeconds++;
-        Seconds.innerHTML='0'+initialSeconds;
         
-        initialMiliSeconds=0;
+        // Update seconds display with leading zero
+        Seconds.innerHTML = initialSeconds <= 9 ? '0' + initialSeconds : initialSeconds;
+        
+        initialMiliSeconds = 0;
         MiliSeconds.innerHTML = '0' + initialMiliSeconds;
     }
-    if(initialSeconds>9){
-        Seconds.innerHTML = initialSeconds;
-    }
-    if(initialSeconds>59){
+    
+    if (initialSeconds > 59) {
         initialMinutes++;
-        Minutes.innerHTML= '0'+initialMinutes;
+        
+        // Update minutes display with leading zero
+        Minutes.innerHTML = initialMinutes <= 9 ? '0' + initialMinutes : initialMinutes;
 
-        initialSeconds=0;
-        Seconds.innerHTML ='0'+initialSeconds;
+        initialSeconds = 0;
+        Seconds.innerHTML = '0' + initialSeconds;
     }
 }
